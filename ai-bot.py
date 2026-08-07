66afd324b17ac218e54ecde6826c3cc5642cb7e4
Saksham-tamera <babacoder045@gmail.com>
2026-08-08 02:05:17 +0530

    Update speech text in ai-bot.py

--- ai-bot.py
+++ ai-bot.py
@@ -3,5 +3,5 @@ import pyttsx3
 engine = pyttsx3.init()
 voices = engine.getProperty("voices")
 engine.setProperty("voice", voices[1].id) #0 is for male and 1 is for female 
-engine.say("Hello my self akhand ")
+engine.say("my self akhand ")
 engine.runAndWait()
