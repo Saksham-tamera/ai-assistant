7d64113a6011c82c99d52c1bc6b6d1b33003a04f
Sakshamtamera <sakshamtamera@gmail.com>
2026-08-09 16:25:46 +0530

    updated code

--- ai-bot.py
+++ ai-bot.py
@@ -1,4 +1,4 @@
-import SpeechRecognition 
+import speech_recognition as sr
 import pyttsx3
 import sounddevice
 import requests
@@ -7,6 +7,6 @@ import datetime
 import os
 import subprocess
 import wikipedia
-import python-dotenv
+from dotenv import python_dotenv
 import openai
 print("All modules imported successfully.")
