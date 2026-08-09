6320a44189af71a745e83b3271a07020f09a5ccb
Sakshamtamera <sakshamtamera@gmail.com>
2026-08-09 15:19:03 +0530

    Updated code

---
+++ ai-bot.py
@@ -0,0 +1,12 @@
+mport SpeechRecognition 
+import pyttsx3
+import sounddevice
+import requests
+import webbrowser
+import datetime
+import os
+import subprocess
+import wikipedia
+import python-dotenv
+import openai
+print("All modules imported successfully.")
