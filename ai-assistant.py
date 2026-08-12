a9fb00eb79353282bd5bf02c69e46e6cfcaabe13
Sakshamtamera <sakshamtamera@gmail.com>
2026-08-12 22:18:48 +0530

    updated code

--- ai-assistant.py
+++ ai-assistant.py
@@ -1,40 +1,36 @@
 import speech_recognition as sr
 import pyttsx3
-from openai import OpenAI
 import os
 from dotenv import load_dotenv
+from google import genai
+
 load_dotenv()
+api_key = os.getenv("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
 
 r= sr.Recognizer()
 engine = pyttsx3.init()
 
 def speak(text):
+    print("assistant:",text)
     engine.say(text)
     engine.runAndWait()
-client = OpenAI(
-    base_url="https://openrouter.ai/api/v1",
-    api_key=os.getenv("OPENROUTER_API_KEY")
-)
+
+
+
+
 
 with sr.Microphone() as source:
     audio=r.listen(source)
 text = r.recognize_google(audio)
 print("say somthing",text)
 
-response = client.chat.completions.create(
-    model="openrouter/free",
-    messages=[
-        {
-            "role": "system",
-            "content":"""give only the important points from the user's question and give the answer.only give the bullent point. 
-              don't give the system process.and any extra line""" 
-        },
-        {"role": "user", "conent": text}
-    ]
-)
-response.choices[0].message.content
+response = client.models.generate_content(
+    model="gemini-3.6-flash",
+    contents=text
+    )
 
+answer = response.text
 
-speak(response)
-
-          
+print("assistant:",answer)
+speak(answer)
