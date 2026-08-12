e0cb7ba77b1620c589afdad80445bb55f8ce92ef
Sakshamtamera <sakshamtamera@gmail.com>
2026-08-13 01:46:44 +0530

    updated code

---
+++ wekipedia.py
@@ -0,0 +1,36 @@
+import speech_recognition as sr
+import pyttsx3
+import wikipediaapi
+
+wiki = wikipediaapi.Wikipedia(
+    language='en',
+    user_agent='AI Assistant/1.0'
+    )
+
+#initialize the recognizer
+r=sr.Recognizer()
+engine = pyttsx3.init()
+
+def speak(text):
+    print("assistant:",text)
+    engine.say(text)
+    engine.runAndWait()
+
+with sr.Microphone() as source:
+    print("Listening...")
+    audio=r.listen(source)
+    audio_text=r.recognize_google(audio)
+    print("You said:",audio_text)
+
+    page = wiki.page(audio_text)
+    your_title = page.title
+    your_summary = page.summary
+    
+    if page.exists():
+        print(your_title)
+        speak(your_title)
+        print(your_summary)
+        
+    else:
+        print("No page found for:", audio_text)
+        speak("No page found for: " + audio_text)
