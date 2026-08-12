c516cb5f66ef049d33949ca0d42b4dd176bb0340
Sakshamtamera <sakshamtamera@gmail.com>
2026-08-12 18:37:46 +0530

    updated code

--- ai-assistant.py
+++ ai-assistant.py
@@ -1,11 +1,40 @@
 import speech_recognition as sr
 import pyttsx3
-from open_ai import OpenAI
+from openai import OpenAI
 import os
 from dotenv import load_dotenv
 load_dotenv()
 
 r= sr.Recognizer()
 engine = pyttsx3.init()
-client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-print(os.getenv("OPENAI_API_KEY") is not None)
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+client = OpenAI(
+    base_url="https://openrouter.ai/api/v1",
+    api_key=os.getenv("OPENROUTER_API_KEY")
+)
+
+with sr.Microphone() as source:
+    audio=r.listen(source)
+text = r.recognize_google(audio)
+print("say somthing",text)
+
+response = client.chat.completions.create(
+    model="openrouter/free",
+    messages=[
+        {
+            "role": "system",
+            "content":"""give only the important points from the user's question and give the answer.only give the bullent point. 
+              don't give the system process.and any extra line""" 
+        },
+        {"role": "user", "conent": text}
+    ]
+)
+response.choices[0].message.content
+
+
+speak(response)
+
+          
