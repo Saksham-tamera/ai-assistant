9d7cbecc135bf49b0d24e67278910e15cd9a3c1c
Sakshamtamera <sakshamtamera@gmail.com>
2026-08-18 01:35:12 +0530

    updated code

--- ai-bot.py
+++ ai-bot.py
@@ -1,14 +1,11 @@
-from openai import api_key
 import speech_recognition as sr
 import webbrowser
 import pyttsx3
 import os
-from dotenv import load_dotenv
-from google import genai
 
-load_dotenv()
-aapi_key = os.getenv("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
+
+
+
 
 recognizer = sr.Recognizer()
 engine = pyttsx3.init()
@@ -55,29 +52,19 @@ if __name__ == "__main__":
                 speak("Yes, I am here. How can I assist you?")
                 # Listen for the next command
                 with sr.Microphone() as source:
-                    print("Activate...")
+                    speak("Activate...")
                     audio = r.listen(source)
                     command = r.recognize_google(audio)
 
-                    response = client.models.generate_content(
-                            model="gemini-3.6-flash",
-                            contents=command
-                    )
-                    answer = response.text
-                    answer = answer.replace("*","")
-                    answer = answer.replace(",","")
-                    answer = answer.replace("#","")
-                    speak(answer)
-                    print(answer)
+                   
             elif "false" in word:
                 speak("goodbye!")
                 break
+
             else:
                     speak("Please say the wake word 'jojo' to activate me.")
 
-
                     processCommand(command)
-
 
         except Exception as e:
             print("Error; {0}".format(e))
